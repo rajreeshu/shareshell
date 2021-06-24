35f58f9d4fc6f07e6445d06efd5a1114773ff1de
ReeshU Raj <reeshu54raj@gmail.com>
2021-06-24 10:00:59 +0530

    forget password work started

--- application/views/website/forget_password.php
+++ application/views/website/forget_password.php
@@ -1,8 +1,5 @@
 <!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<html class="no-js">
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -11,79 +8,63 @@
         <meta name="author" content="Kimarotec">
         <meta name="keyword" content="html5, css, bootstrap, property, real-estate theme , bootstrap template">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <!-- <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700,800' rel='stylesheet' type='text/css'> -->
-
-        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
-<!--         <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
-        <link rel="icon" href="favicon.ico" type="image/x-icon">
-
-        <link rel="stylesheet" href="assets/css/normalize.css">
-        <link rel="stylesheet" href="assets/css/font-awesome.min.css">
-        <link rel="stylesheet" href="assets/css/fontello.css">
-        <link href="assets/fonts/icon-7-stroke/css/pe-icon-7-stroke.css" rel="stylesheet">
-        <link href="assets/fonts/icon-7-stroke/css/helper.css" rel="stylesheet">
-        <link href="assets/css/animate.css" rel="stylesheet" media="screen">
-        <link rel="stylesheet" href="assets/css/bootstrap-select.min.css"> 
-        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-        <link rel="stylesheet" href="assets/css/icheck.min_all.css">
-        <link rel="stylesheet" href="assets/css/price-range.css">
-        <link rel="stylesheet" href="assets/css/owl.carousel.css">  
-        <link rel="stylesheet" href="assets/css/owl.theme.css">
-        <link rel="stylesheet" href="assets/css/owl.transitions.css">
-        <link rel="stylesheet" href="assets/css/style.css">
-        <link rel="stylesheet" href="assets/css/responsive.css"> -->
     </head>
     <body>
 <?php
     $this->load->view('website/link_import');
     $this->load->view('website/header');
 ?> 
-
-      
         <!-- End of nav bar -->
 
         <div class="page-head"> 
             <div class="container">
                 <div class="row">
                     <div class="page-head-content">
-                        <h1 class="page-title">Hello : <span class="orange strong">YOUR NAME</span></h1>               
+                        <h1 class="page-title">RESET YOUR <span class="orange strong">PASSWORD</span></h1>               
                     </div>
                 </div>
             </div>
         </div>
         <!-- End page header --> 
-
-        <!-- property area -->
         <div class="content-area user-profiel" style="background-color: #FCFCFC;">&nbsp;
-            <div class="container">   
-                <div class="row">
-                    <div class="col-sm-10 col-sm-offset-1 profiel-container">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-10 col-sm-offset-1 profiel-container">
 
-                        <form action="" method="">
+                    <form action="" method="">
 
-                            <div class="profiel-header">
-                                <h3>
-                                    <b>UPDATE</b> YOUR PASSWORD <br>
-                                    <small>All change will send to your e-mail.</small>
-                                </h3>
-                                <hr>
-                            </div>
+                        <div class="profiel-header">
+                            <h3>
+                                <b>UPDATE</b> YOUR PASSWORD <br>
+                                <small>All change will send to your e-mail.</small>
+                            </h3>
+                            <hr>
+                        </div>
 
-                            <div class="clear">
+                        <div class="clear">
 
-                                <div class="col-sm-10 col-sm-offset-1">
-                                    <div class="form-group">
-                                        <label>Enter email/ph no <small>(required)</small></label>
-                                        <input name="Password" type="password" class="form-control">
+                            <div class="col-sm-10 col-sm-offset-1">
+                                <div class="form-group">
+                                    <label>Enter email/ph no <small>(required)</small></label>
+                                    <input name="Password" type="password" class="form-control">
+                                    <div class="col-sm-0 col-sm-offset-1">
+                                        <input id="btn1" type='button' class='btn btn-finish btn-primary pull-right'
+                                            name='Enter' value='Enter Email' onclick="showDivs('btn1','','display1')" />
                                     </div>
-                                    <div class="form-group" >
-                                        <label>Enter OTP sent to your mail/ph:<small>(required)</small></label>
-                                        <input type="password" class="form-control">
-                                        <div>
-                                            <li><a href="forget_password.html">Resend</a>  </li>
-                                        </div>
+                                </div>
+                                <div class="form-group" id="display1" style="display: none;" >
+                                    <label>Enter OTP sent to your mail/ph:<small>(required)</small> 
+                                        <a href="forget_password.html">Resend OTP</a> </label>
+                                    <input  type="password" class="  col-sm-6 form-control">
+                                    <div class="col-sm-0 col-sm-offset-1">
+                                        <input id="btn2" type='button' class='btn btn-finish btn-primary pull-right'
+                                            name='Enter  OTP' value='Enter OTP'  onclick='showDivs("btn2","display1","display2")' />
                                     </div>
+
+
+
+                                </div>
+                                <div id="display2" style="display: none;">
                                     <div class="form-group">
                                         <label>New Password <small>(required)</small></label>
                                         <input name="Password" type="password" class="form-control">
@@ -91,17 +72,20 @@
                                     <div class="form-group">
                                         <label>Confirm New password : <small>(required)</small></label>
                                         <input type="password" class="form-control">
-                                    </div> 
+                                    </div>
+                                    <div class="col-sm-0 col-sm-offset-1">
+                                        <input type='button' class='btn btn-finish btn-primary pull-right' name='update'
+                                            value='Update'/>
+                                    </div>
                                 </div>
-                                <div class="col-sm-10 col-sm-offset-1">
-                                    <input type='button' class='btn btn-finish btn-primary pull-right' name='update' value='Update' />
-                                </div>
-                                
                             </div>
- 
-                    
                             
-                            
+
+                        </div>
+
+
+
+
                     </form>
 
                 </div>
@@ -116,23 +100,17 @@
     $this->load->view('website/js_import');
 ?>
 
-<!--         <script src="assets/js/modernizr-2.6.2.min.js"></script>
-
-        <script src="assets/js/jquery-1.10.2.min.js"></script> 
-        <script src="bootstrap/js/bootstrap.min.js"></script>
-        <script src="assets/js/bootstrap-select.min.js"></script>
-        <script src="assets/js/bootstrap-hover-dropdown.js"></script>
-
-        <script src="assets/js/easypiechart.min.js"></script>
-        <script src="assets/js/jquery.easypiechart.min.js"></script>
-
-        <script src="assets/js/owl.carousel.min.js"></script>
-        <script src="assets/js/wow.js"></script>
-
-        <script src="assets/js/icheck.min.js"></script>
-        <script src="assets/js/price-range.js"></script>
-
-        <script src="assets/js/main.js"></script>
- -->
     </body>
+
+<script>
+
+    function showDivs(btn, toHide, toShow){
+            document.getElementById(toShow).style.display = "block";
+            document.getElementById(btn).style.display = "none";
+            if(toHide!=""){
+                document.getElementById(toHide).style.display = "none";
+            }
+    }
+</script>
+
 </html>
